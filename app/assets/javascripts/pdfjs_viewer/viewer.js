0d6cd719b67bb1e12fb15e58569b16ebb04fafc2
Adam Derewecki <adam@snapdocs.com>
2017-02-15 15:16:06 -0800

    Remove validateFileURL checking for same hostname

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -7179,7 +7179,7 @@ function webViewerInitialized() {
   var queryString = document.location.search.substring(1);
   var params = parseQueryString(queryString);
   var file = 'file' in params ? params.file : DEFAULT_URL;
-  validateFileURL(file);
+  // validateFileURL(file);
 
   var fileInput = document.createElement('input');
   fileInput.id = 'fileInput';
