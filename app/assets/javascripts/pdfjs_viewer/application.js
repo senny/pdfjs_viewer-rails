2a2fd2a4138b7074c5defdce60200c3890fa3f7e
gkumarantrix <gkumar@gomoxie.com>
2018-02-14 14:48:14 +0530

    update_pdfjs

--- app/assets/javascripts/pdfjs_viewer/application.js
+++ app/assets/javascripts/pdfjs_viewer/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require pdfjs_viewer/pdfjs/compatibility
-//= require pdfjs_viewer/pdfjs/l10n
 //= require pdfjs_viewer/pdfjs/pdf
 //= require pdfjs_viewer/viewer_configurations
 //= require pdfjs_viewer/viewer
