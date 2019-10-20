75b002d27eb8d7d07c8892e3ecf03e54a08b61f3
IanPurdom <ianpurdom75@gmail.com>
2019-10-20 18:15:21 +0200

    update hosted viewer

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -1886,7 +1886,14 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 
+                              'http://mozilla.github.io', 
+                              'https://mozilla.github.io', 
+                              'https://localhost3000',
+                              'http://localhost3000',
+                              'https://comptes2campagnes.herokuapp.com',
+                              'http://comptes2campagnes.herokuapp.com'
+                            ];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
