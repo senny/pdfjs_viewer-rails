ff5ea464cbbb559662759fb8d98940d594f37c04
WonjaeLee <30612533+kinmt1234@users.noreply.github.com>
2020-07-09 09:59:58 +0900

    Update viewer.js

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -1896,10 +1896,10 @@ var validateFileURL = void 0;
       if (HOSTED_VIEWER_ORIGINS.indexOf(viewerOrigin) >= 0) {
         return;
       }
-      var fileOrigin = new URL(file, window.location.href).origin;
-      if (fileOrigin !== viewerOrigin) {
-        throw new Error('file origin does not match viewer\'s');
-      }
+//       var fileOrigin = new URL(file, window.location.href).origin;
+//       if (fileOrigin !== viewerOrigin) {
+//         throw new Error('file origin does not match viewer\'s');
+//       }
     } catch (ex) {
       var message = ex && ex.message;
       PDFViewerApplication.l10n.get('loading_error', null, 'An error occurred while loading the PDF.').then(function (loadingErrorMessage) {
