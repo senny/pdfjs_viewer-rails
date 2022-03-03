3ae44c698d1d10a68ad0a5db6bcb1b52a309df6e
cdapalc <cdapalc@hotmail.com>
2022-03-03 17:01:02 +0100

    create page param

--- app/assets/javascripts/pdfjs_viewer/pdfjs/pdf.combined.js
+++ app/assets/javascripts/pdfjs_viewer/pdfjs/pdf.combined.js
@@ -47946,13 +47946,12 @@ include('https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', func
       // decodeURIComponent(window.location.search.substring(1)).split('&')[1].split('=')[1]
       // var page = getUrlParameter('page');
       
-
-
       setTimeout(function() {
         console.log(window.location.search.substring(1).split('&')[1].split('=')[1]);
         if (window.location.search.substring(1).split('&')[1].split('=')[1] != undefined) {
           //FIND BAR VALUE SET TO QUERY INPUT
-          $("#pageNumber").val(window.location.search.substring(1).split('&')[1].split('=')[1]);
+          // $("#pageNumber").val(window.location.search.substring(1).split('&')[1].split('=')[1]);
+          $("#pageNumber").val("7");
           //AUTO CLICK NEXT BUTTON
           $("#pageNumber").trigger('click');
         }
