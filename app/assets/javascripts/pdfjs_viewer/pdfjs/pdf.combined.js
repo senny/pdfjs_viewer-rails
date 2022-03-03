a6a1db070b8942211f12a969312d7bf0b7e8988d
cdapalc <cdapalc@hotmail.com>
2022-03-03 16:55:49 +0100

    create page param

--- app/assets/javascripts/pdfjs_viewer/pdfjs/pdf.combined.js
+++ app/assets/javascripts/pdfjs_viewer/pdfjs/pdf.combined.js
@@ -47945,19 +47945,20 @@ include('https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', func
    
       // decodeURIComponent(window.location.search.substring(1)).split('&')[1].split('=')[1]
       // var page = getUrlParameter('page');
-      console.log(window.location.href.split('&')[1].split('=')[1])
-
       
 
+
+      setTimeout(function() {
+        console.log(window.location.search.substring(1).split('&')[1].split('=')[1]);
+        if (window.location.search.substring(1).split('&')[1].split('=')[1] != undefined) {
+          //FIND BAR VALUE SET TO QUERY INPUT
+          $("#pageNumber").val(window.location.search.substring(1).split('&')[1].split('=')[1]);
+          //AUTO CLICK NEXT BUTTON
+          $("#pageNumber").trigger('click');
+        }
+      }, 5000);
+
       
-      if (window.location.href.split('&')[1].split('=')[1] != undefined){
-        
-        //FIND BAR VALUE SET TO QUERY INPUT
-        $("#pageNumber").val(window.location.search.substring(1).split('&')[1].split('=')[1]);
-        //AUTO CLICK NEXT BUTTON
-        $("#pageNumber").trigger('click');
-        
-      }
 
 
   });
