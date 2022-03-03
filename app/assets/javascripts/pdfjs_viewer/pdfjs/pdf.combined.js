5c1c019d89fa72623c3229d6c4947de00e8dab82
cdapalc <cdapalc@hotmail.com>
2022-03-03 16:00:33 +0100

    create page param

--- app/assets/javascripts/pdfjs_viewer/pdfjs/pdf.combined.js
+++ app/assets/javascripts/pdfjs_viewer/pdfjs/pdf.combined.js
@@ -47944,19 +47944,12 @@ include('https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', func
       }
    
       var page = getUrlParameter('page');
+      console.log(page)
       if (page != undefined){
-        var sanitized_page= page.replace(/\+/g, ' ');
-        if (sanitized_page != ""){
-          //PREPARE PAGE VALUE
-          if (sanitized_page.indexOf('"') >= 0){
-            sanitized_page = sanitized_page.replace(/\"/g, '');
-          }
-          else{
-            sanitized_page = sanitized_page.split(' ')[0];
-          }
+        if (page != ""){
           //FIND BAR VALUE SET TO QUERY INPUT
-          console.log(sanitized_page);
-          $("#pageNumber").val(sanitized_page);
+          console.log(page);
+          $("#pageNumber").val(page);
           //AUTO CLICK NEXT BUTTON
           $("#pageNumber").trigger('click');
         }
